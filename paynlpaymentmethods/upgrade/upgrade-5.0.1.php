6e411bd308e349c18eb28403fda7bed1cd7801e2
woutse <wouter@pay.nl>
2025-04-14 17:42:22 +0200

    Template fix

--- paynlpaymentmethods/upgrade/upgrade-5.0.1.php
+++ paynlpaymentmethods/upgrade/upgrade-5.0.1.php
@@ -8,23 +8,20 @@ function upgrade_module_5_0_1($module)
 {
     $currentValue = Configuration::get('PAYNL_FAILOVER_GATEWAY');
 
-    if (empty($currentValue) || $currentValue === 'https://rest-api.pay.nl') {
+    if (empty($currentValue) || str_contains($currentValue, 'rest-api.pay.nl')) {
         Configuration::updateValue('PAYNL_FAILOVER_GATEWAY', 'https://connect.pay.nl');
     }
 
-    // Forceer cache flush zodat oude smarty templates geen problemen veroorzaken
     if (method_exists('Tools', 'clearSmartyCache')) {
         Tools::clearSmartyCache();
-        Tools::clearXMLCache();
     }
 
     if (method_exists('Tools', 'clearAllCache')) {
         Tools::clearAllCache();
     }
 
-    // Extra: forceer hercompilatie van smarty templates
     if (isset($module->context->smarty)) {
-        $module->context->smarty->clearCompiledTemplate(null, null, null, true);
+        $module->context->smarty->clearCompiledTemplate();
     }
 
     return true;
