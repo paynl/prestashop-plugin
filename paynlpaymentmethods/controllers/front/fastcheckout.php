b733bbc35c66ab2bafb9c04049d8ec4ab937f1ac
Kevin Verschoor <k.verschoor@pay.nl>
2025-11-07 14:46:56 +0100

    Load latest cart if empty

--- paynlpaymentmethods/controllers/front/fastcheckout.php
+++ paynlpaymentmethods/controllers/front/fastcheckout.php
@@ -7,14 +7,38 @@ class PaynlPaymentMethodsFastcheckoutModuleFrontController extends ModuleFrontCo
      */
     public function postProcess()
     {
-        $cart = $this->context->cart;
+        try {
+            $cart = $this->context->cart;
+            if (empty($cart->id) || !Validate::isLoadedObject($cart)) {
+                $latestCartId = $this->getLatestCartId();
+                $cart = new Cart($latestCartId);
+            }
 
-        $this->context->cart->id_customer = 0;
-        $this->context->cart->id_address_delivery = 0;
-        $this->context->cart->id_address_invoice = 0;
-        $this->context->cart->update();
+            $cart->id_customer = 0;
+            $cart->id_address_delivery = 0;
+            $cart->id_address_invoice = 0;
+            $cart->update();
 
-        $transaction = $this->module->startPayment($cart, 10, ['fastcheckout' => true]);
-        Tools::redirect($transaction);
+            $this->context->cart = $cart;
+
+            $transaction = $this->module->startPayment($cart, 10, ['fastcheckout' => true]);
+            Tools::redirect($transaction);
+        } catch (Throwable $th) {
+            $link = Context::getContext()->link;
+            $refererUrl = $link->getRefererURL();
+            Tools::redirect($refererUrl);
+        }
+    }
+
+    function getLatestCartId(): int
+    {
+        $db = Db::getInstance();
+        $sql = '
+            SELECT MAX(id_cart)
+            FROM `' . _DB_PREFIX_ . 'cart`
+            ORDER BY id_cart DESC
+        ';
+        $latestCartId = $db->getValue($sql);
+        return (int) $latestCartId;
     }
 }
