98953de9230e9ab62067ac332fe9205e33acaac2
Kevin Verschoor <k.verschoor@pay.nl>
2025-11-07 15:02:56 +0100

    Update fastcheckout.php

--- paynlpaymentmethods/controllers/front/fastcheckout.php
+++ paynlpaymentmethods/controllers/front/fastcheckout.php
@@ -7,12 +7,10 @@ class PaynlPaymentMethodsFastcheckoutModuleFrontController extends ModuleFrontCo
      */
     public function postProcess()
     {
+        $this->helper = new PayHelper();
+
         try {
             $cart = $this->context->cart;
-            if (empty($cart->id) || !Validate::isLoadedObject($cart)) {
-                $latestCartId = $this->getLatestCartId();
-                $cart = new Cart($latestCartId);
-            }
 
             $cart->id_customer = 0;
             $cart->id_address_delivery = 0;
@@ -24,21 +22,11 @@ class PaynlPaymentMethodsFastcheckoutModuleFrontController extends ModuleFrontCo
             $transaction = $this->module->startPayment($cart, 10, ['fastcheckout' => true]);
             Tools::redirect($transaction);
         } catch (Throwable $th) {
+            $this->helper->payLog('fastcheckout', 'could not start fast checkout: ' . $th->getMessage(), null, null);
+
             $link = Context::getContext()->link;
             $refererUrl = $link->getRefererURL();
             Tools::redirect($refererUrl);
         }
-    }
-
-    function getLatestCartId(): int
-    {
-        $db = Db::getInstance();
-        $sql = '
-            SELECT MAX(id_cart)
-            FROM `' . _DB_PREFIX_ . 'cart`
-            ORDER BY id_cart DESC
-        ';
-        $latestCartId = $db->getValue($sql);
-        return (int) $latestCartId;
     }
 }
