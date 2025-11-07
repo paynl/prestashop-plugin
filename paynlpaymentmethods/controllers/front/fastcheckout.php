53b9913d9815ccf255878cd8a59f9c199437cd82
Kevin Verschoor <k.verschoor@pay.nl>
2025-11-07 15:06:40 +0100

    Update fastcheckout.php

--- paynlpaymentmethods/controllers/front/fastcheckout.php
+++ paynlpaymentmethods/controllers/front/fastcheckout.php
@@ -7,26 +7,24 @@ class PaynlPaymentMethodsFastcheckoutModuleFrontController extends ModuleFrontCo
      */
     public function postProcess()
     {
-        $this->helper = new PayHelper();
-
         try {
+
             $cart = $this->context->cart;
 
-            $cart->id_customer = 0;
-            $cart->id_address_delivery = 0;
-            $cart->id_address_invoice = 0;
-            $cart->update();
-
-            $this->context->cart = $cart;
+            $this->context->cart->id_customer = 0;
+            $this->context->cart->id_address_delivery = 0;
+            $this->context->cart->id_address_invoice = 0;
+            $this->context->cart->update();
 
             $transaction = $this->module->startPayment($cart, 10, ['fastcheckout' => true]);
             Tools::redirect($transaction);
+
         } catch (Throwable $th) {
-            $this->helper->payLog('fastcheckout', 'could not start fast checkout: ' . $th->getMessage(), null, null);
 
             $link = Context::getContext()->link;
             $refererUrl = $link->getRefererURL();
             Tools::redirect($refererUrl);
+
         }
     }
 }
