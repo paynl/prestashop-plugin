c70a32f236fcffe6cab20bed0783287ba6d226d2
woutse <wouter@pay.nl>
2026-05-29 14:59:37 +0200

    Fixed error on setting language

--- paynlpaymentmethods/paynlpaymentmethods.php
+++ paynlpaymentmethods/paynlpaymentmethods.php
@@ -1281,7 +1281,7 @@ class PaynlPaymentMethods extends PaymentModule
         $requestOrderData = new PayNL\Sdk\Model\Order();
 
         if (empty($parameters['fastcheckout']) || $parameters['fastcheckout'] !== true) {
-            $request->setCustomer($this->addressHelper->getCustomer($cart));
+            $request->setCustomer($this->addressHelper->getCustomer($cart, $this));
 
             $requestOrderData->setInvoiceAddress($this->addressHelper->getInvoiceAddress($cart));
             $requestOrderData->setDeliveryAddress($this->addressHelper->getDeliveryAddress($cart));
@@ -1589,65 +1589,7 @@ class PaynlPaymentMethods extends PaymentModule
 
 
 
-    /**
-     * @return string
-     */
-    private function getBrowserLanguage()
-    {
-        if (isset($_SERVER["HTTP_ACCEPT_LANGUAGE"])) {
-            return $this->parseDefaultLanguage($_SERVER["HTTP_ACCEPT_LANGUAGE"]);
-        } else {
-            return $this->parseDefaultLanguage(null);
-        }
-    }
 
-    /**
-     * @param string $http_accept
-     * @param string $deflang
-     * @return string
-     */
-    private function parseDefaultLanguage($http_accept, $deflang = "en")
-    {
-        if (isset($http_accept) && strlen($http_accept) > 1) {
-            $lang = array();
-            # Split possible languages into array
-            $x = explode(",", $http_accept);
-            foreach ($x as $val) {
-                #check for q-value and create associative array. No q-value means 1 by rule
-                if (preg_match(
-                  "/(.*);q=([0-1]{0,1}.[0-9]{0,4})/i",
-                  $val,
-                  $matches
-                )) {
-                    $lang[$matches[1]] = (float)$matches[2] . '';
-                } else {
-                    $lang[$val] = 1.0;
-                }
-            }
-
-            $arrLanguages = $this->getLanguages();
-            $arrAvailableLanguages = array();
-            foreach ($arrLanguages as $language) {
-                if ($language['language_id'] != 'auto') {
-                    $arrAvailableLanguages[] = $language['language_id'];
-                }
-            }
-
-            #return default language (highest q-value)
-            $qval = 0.0;
-            foreach ($lang as $key => $value) {
-                $languagecode = strtolower(substr($key, 0, 2));
-                if (in_array($languagecode, $arrAvailableLanguages)) {
-                    if ($value > $qval) {
-                        $qval = (float)$value;
-                        $deflang = $key;
-                    }
-                }
-            }
-        }
-
-        return strtolower(substr($deflang, 0, 2));
-    }
 
     /**
      * @return array
